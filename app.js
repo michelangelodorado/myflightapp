2d0f502a1033bf312ddb2ee5c862d82be8c840f6
Mike <102953584+michelangelodorado@users.noreply.github.com>
2023-12-08 18:45:45 +0800

    Update app.js

--- app.js
+++ app.js
@@ -6,100 +6,83 @@ const port = 8080;
 app.use(express.json());
 
 // Sample data
-const users = [
-  { id: 1, firstname: 'Mike', surname: 'Dorado', number: '943xxx43'  },
-  { id: 2, firstname: 'Zacha', surname: 'Valerie', number: '861xxx62'  },
-  { id: 3, firstname: 'Yuki', surname: 'Dorado', number: '861xxx63'  },
+const flights = [
+  { id: 1, origin: 'SIN', destination: 'MNL', airlines: 'PAL'  },
+  { id: 2, origin: 'SIN', destination: 'MNL', airlines: 'CEB'  },
+  { id: 3, origin: 'SIN', destination: 'MNL', airlines: 'SQ'  },
 ];
 
-const roles = [
-  { id: 1, role: 'Solutions Engineer' },
-  { id: 2, role: 'Software QA'  },
-];
-
-// Route to get all users
-app.get('/users', (req, res) => {
-//  res.json(users);
+// Route to get all flights
+app.get('/getFlight', (req, res) => {
+//  res.json(flights);
         // Use JSON.stringify with null and 2 for indentation
-  const formattedUsers = JSON.stringify(users, null, 2);
+  const formattedFlights = JSON.stringify(flights, null, 2);
 
   // Set Content-Type header to application/json
   res.setHeader('Content-Type', 'application/json');
 
   // Send the nicely formatted JSON response
-  res.end(formattedUsers);
+  res.end(formattedFlights);
 });
 
-// Route to get all roles
-app.get('/roles', (req, res) => {
-  //res.json(roles);
-  const formattedUsers = JSON.stringify(roles, null, 2);
+// Route to get a specific flight by ID
+app.get('/getFlight/:id', (req, res) => {
+  const flightId = parseInt(req.params.id);
+  const flight = flights.find(u => u.id === flightId);
+ // const formattedflight = JSON.stringify(flight, null, 2);
 
-  // Set Content-Type header to application/json
-  res.setHeader('Content-Type', 'application/json');
-
-  // Send the nicely formatted JSON response
-  res.end(formattedUsers);
-});
-
-// Route to get a specific user by ID
-app.get('/users/:id', (req, res) => {
-  const userId = parseInt(req.params.id);
-  const user = users.find(u => u.id === userId);
- // const formatteduser = JSON.stringify(user, null, 2);
-
-  if (!user) {
-    return res.status(404).json({ error: 'User not found' });
+  if (!flight) {
+    return res.status(404).json({ error: 'Flight not found' });
   }
   res.setHeader('Content-Type', 'application/json');
-  res.json(user);
+  res.json(flight);
 });
 
-// Route to create a new user
-app.post('/users', (req, res) => {
-  const newUser = req.body;
-  if (!isValidUser(newUser)) {
-    return res.status(400).json({ error: 'Bad Request. Invalid user data.' });
+// Route to create a new flight
+app.post('/createFlight', (req, res) => {
+  const newFlight = req.body;
+  if (!isValidFlight(newFlight)) {
+    return res.status(400).json({ error: 'Bad Request. Invalid flight data.' });
   }
-  newUser.id = users.length + 1;
-  users.push(newUser);
+  newFlight.id = flights.length + 1;
+  flights.push(newFlight);
 
-  res.status(201).json(newUser);
+  res.status(201).json(newFlight);
 });
 
-function isValidUser(user) {
+function isValidFlight(flight) {
   // Implement your validation logic here
   // For example, check if the required fields are present
-  return user && user.firstname && user.surname && user.number;
+  return flight && flight.firstname && flight.surname && flight.number;
 }
 
-// Route to update a user by ID
-app.put('/users/:id', (req, res) => {
-  const userId = parseInt(req.params.id);
-  const user = users.find(u => u.id === userId);
+// Route to update a flight by ID
+app.put('/updateFlight/:id', (req, res) => {
+  const flightId = parseInt(req.params.id);
+  const flight = flights.find(u => u.id === flightId);
 
-  if (!user) {
-    return res.status(404).json({ error: 'User not found' });
+  if (!flight) {
+    return res.status(404).json({ error: 'Flight not found' });
   }
 
-  // Update user data
-  user.name = req.body.name;
-  res.json(user);
+  // Update flight data
+  flight.name = req.body.name;
+  res.json(flight);
 });
 
-// Route to delete a user by ID
-app.delete('/users/:id', (req, res) => {
-  const userId = parseInt(req.params.id);
-  const index = users.findIndex(u => u.id === userId);
+// Route to delete a flight by ID
+app.delete('/deleteFlights/:id', (req, res) => {
+  const flightId = parseInt(req.params.id);
+  const index = flights.findIndex(u => u.id === flightId);
 
   if (index === -1) {
-    return res.status(404).json({ error: 'User not found' });
+    return res.status(404).json({ error: 'Flight not found' });
   }
 
-  // Remove user from the array
-  users.splice(index, 1);
+  // Remove flight from the array
+  flights.splice(index, 1);
 
-  res.json({ message: 'User deleted successfully' });
+  res.json({ message: 'Flight deleted successfully' });
 });
 
 app.listen(port, () => {
